f6f81c060ae7020b08a74db4086d1ef3b20f13e4
parthshah05 <parth.babu05@gmail.com>
2020-09-09 10:26:52 +0530

    Register and login with fake apis

--- src/components/signin.js
+++ src/components/signin.js
@@ -1,8 +1,47 @@
-import React  from "react";
+import React,{useState}  from "react";
 import Headers from "./header";
-//import { Link } from "react-router-dom";
+import {signin} from "../containers/signin";
 
 const Signin = () => {
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+    error: "",
+    success: false
+  });
+
+  const { email, password, error, success } = values;
+  
+
+  const handleChange = email => event => {
+    setValues({ ...values, error: false, [email]: event.target.value });
+  };
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({ ...values, error: false });
+    signin({ email, password })
+      .then(data => {
+        if (data.error) {
+          setValues({ ...values, error: data.error,success: false});
+        } else {
+          setValues({
+            ...values,
+            email: "",
+            password: "",
+            error: "",
+            success: true
+          });
+          // authenticate(data, () => {
+          //   setValues({
+          //     ...values,
+          //     didRedirect: true
+          //   });
+          // });
+        }
+      })
+  };
+  
   const signInForm = () => {
     return (
       <div className="row">
@@ -10,15 +49,46 @@ const Signin = () => {
           <form>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email" />
+              <input className="form-control" type="email" onChange={handleChange("email")}
+                value={email} />
             </div>
 
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" />
+              <input className="form-control"  onChange={handleChange("password")}
+                value={password} type="password" />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={onSubmit} className="btn btn-success btn-block">Submit</button>
           </form>
+        </div>
+      </div>
+    );
+  };
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            Login successfully. 
+           
+          </div>
+        </div>
+      </div>
+    );
+  };
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
         </div>
       </div>
     );
@@ -26,6 +96,8 @@ const Signin = () => {
 
   return (
     <Headers title="Sign In page" description="A page for user to sign in!">
+    {successMessage()}
+    {errorMessage()}
       {signInForm()}
     </Headers>
   );
